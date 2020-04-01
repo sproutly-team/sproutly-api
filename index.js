fe83119c8be529303b2e3b7908bb157f698cd323
Chinedu Ebinim <chinedu@verifi.ng>
2020-04-01 10:57:52 +0100

    make env variables optional

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const dotenv = require("dotenv-safe");
 
 dotenv.config({});
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000;
 
 const db = require('./config/queries');
 Sentry.init({
