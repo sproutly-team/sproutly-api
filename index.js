b4ec3504c96d91220bf20a79a5aa058900d80524
Chinedu Ebinim <chinedu@verifi.ng>
2020-04-02 13:11:01 +0100

    cors- allow all origins

--- index.js
+++ index.js
@@ -12,18 +12,8 @@ const db = require('./config/queries');
 Sentry.init({
   dsn: process.env.SENTRY_URL
 });
-var whitelist = ["https://sproutly.netlify.com"];
-var corsOptions = {
-  origin: function(origin, callback) {
-    if (whitelist.indexOf(origin) !== -1) {
-      callback(null, true);
-    } else {
-      callback(new Error("Not allowed by CORS"));
-    }
-  }
-};
- 
-app.use(cors(corsOptions));
+
+app.use(cors());
 app.use(bodyParser.json())
 app.use(
   bodyParser.urlencoded({
