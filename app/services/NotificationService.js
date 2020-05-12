8641cb1faac14862cbaf40c54f9821bf01e3fe7d
Theo <theo@Theos-MacBook-Air.local>
2020-05-12 03:38:16 +0100

    use email

--- app/services/NotificationService.js
+++ app/services/NotificationService.js
@@ -3,7 +3,7 @@ const MailerService = require('./MailerService')
 const service = {
   async newSignup({ email, code, firstname }) {
     const mailPayload = {
-      to: 'pietrosparks@gmail.com',
+      to: email,
       subject: 'Welcome to Sproutly',
       payload: `Hi ${firstname}, Welcome to Sproutly. Your Confirmation Code is ${code}. Please click the link below (coming soon) to complete onboarding`
     }
